58a940c897dc39e5b907b78c9db902a6927beb01
Iñigo Aguas Ardaiz <inigo.aguasardaiz@edu.esiee.fr>
2017-01-30 23:49:12 +0100

    Report and other little changes. Version submitted to ESIEE.

--- project/test-cga.sh
+++ project/test-cga.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 # Script to test all the functions
 
+make
+
 mkdir -p img/cga/out
 echo "Starting all tests..."
 echo "Executing average filter"
 ./bin/avgfilter img/cga/lena_grey_noisy.pgm img/cga/out/lenaGray-avgfilter-3.pgm 3
 ./bin/avgfilter_color img/cga/lena_std_noisy.ppm img/cga/out/lenaColor-avgfilter-3.ppm 3
+./bin/avgfilter img/cga/lena_grey_noisy.pgm img/cga/out/lenaGray-avgfilter-5.pgm 5
+./bin/avgfilter_color img/cga/lena_std_noisy.ppm img/cga/out/lenaColor-avgfilter-5.ppm 5
 echo "Executing pixelwise filter"
 ./bin/pixelwisefilter img/cga/lena_grey_noisy.pgm img/cga/out/lenaGray-pixelwisefilter-10.pgm 10
 ./bin/pixelwisefilter img/cga/lena_grey_noisy.pgm img/cga/out/lenaGray-pixelwisefilter-40.pgm 40
